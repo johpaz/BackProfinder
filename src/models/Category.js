fae0242dd6410d20c24fd7a693be91cf2671c60f
John <johpaz252@gmail.com>
2023-07-16 22:32:33 -0500

    firebase

--- src/models/Category.js
+++ src/models/Category.js
@@ -3,7 +3,7 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define("Category",
   {
-    id:{
+    id:{ 
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
