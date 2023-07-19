8ee4e484e73c8f5246fb8a3da3c56579fc72a20e
John <johpaz252@gmail.com>
2023-07-18 23:51:17 -0500

    correccioon carpeta

--- src/src/controllers/postsClientControllers/index.js
+++
@@ -1,7 +0,0 @@
-const {getAllPostsByClients,getAllPostsByClientsApi} = require("./01 - getAllPostsByClients");
-const createPost = require("./02 - createPost")
-
-module.exports = { getAllPostsByClients,
-    getAllPostsByClientsApi,
-    createPost
- };
