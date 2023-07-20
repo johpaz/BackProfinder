6cf7edb8c00641deb2e54f50159ed355d8129ac9
John <johpaz252@gmail.com>
2023-07-20 11:05:24 -0500

    act page succes cash

--- src/pasarelapagos/controllers/pasarelaPreferenciaController.js
+++ src/pasarelapagos/controllers/pasarelaPreferenciaController.js
@@ -41,8 +41,8 @@ async function crearPreferencia(req, res, next) {
           ProfesionalId: Number(ProfesionalId),
         },
        ], back_urls : {
-          success: `http://localhost:5173/pasarela`,
-          failure: 'http://localhost:5173/pasarela',
+          success: `http://localhost:5173/dashboardSuppliers`,
+          failure: 'http://localhost:5173/dashboardSuppliers',
           pending: '',
         }, 
         auto_return: 'approved',
