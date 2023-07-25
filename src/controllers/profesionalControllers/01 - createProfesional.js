c7c612946891fcf971d09f9268351cb51e82038e
John <johpaz252@gmail.com>
2023-07-25 17:49:56 -0500

    map

--- src/controllers/profesionalControllers/01 - createProfesional.js
+++ src/controllers/profesionalControllers/01 - createProfesional.js
@@ -42,15 +42,14 @@ const createProfesional = async (name,email,password,image,genre,years_exp,categ
   // console.log(resolvedCategories.map((category)=>category.name))
   // console.log(resolvedCategories.map((category)=> category.ocupations.map((ocupation)=>ocupation.name)));
   //! Que coincidan los id del país y location en la base de datos
-  
+  const CountryId = country.id;
   const country = await Country.findByPk(CountryId);
- // Buscar la ubicación por el LocationId en la base de datos
- const location = await Location.findByPk(LocationId);
- if (!location) {
-   throw new Error(`La ubicación con el ID ${LocationId} no existe en la base de datos`);
- }
-
-
+  const LocationId = location.id;
+  const location = await Location.findByPk(LocationId);
+  if (!location) {
+    throw new Error(`La ubicación con el ID ${LocationId} no existe en la base de datos`);
+  }
+  
   const latitude = location.latitude;
   const longitude = location.longitude;
 
