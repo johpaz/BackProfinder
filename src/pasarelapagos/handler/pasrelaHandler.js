2adc4e1f6508bea0b4e947ca32ac9d2163cace15
John <johpaz252@gmail.com>
2023-07-25 17:05:51 -0500

    1

--- src/pasarelapagos/handler/pasrelaHandler.js
+++
@@ -1,7 +0,0 @@
-const pasarelaController = require('../controllers/pasarelaPreferenciaController');
-
-function pasarelaHandler(req, res, next) {
-  pasarelaController.crearPreferencia(req, res, next);
-}
-
-module.exports = pasarelaHandler;
