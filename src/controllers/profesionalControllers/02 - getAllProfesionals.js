b6f6af500bf350551ab36c5e3193ee3ac2cf2ec1
John <johpaz252@gmail.com>
2023-07-26 17:11:07 -0500

    id

--- src/controllers/profesionalControllers/02 - getAllProfesionals.js
+++ src/controllers/profesionalControllers/02 - getAllProfesionals.js
@@ -92,7 +92,10 @@ const getAllProfesionals = async () => {
           attributes: ["id", "name", "CategoryId"],
           through: { attributes: [] }
         },
-       
+        {
+          model: PostProfesional,
+          attributes: ["id", "title", "image", "content", "softDelete"],
+        },
          {model: Review,
           attributes: ["id","content", "rating"]
         }
